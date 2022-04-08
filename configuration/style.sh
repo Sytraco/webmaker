e72273b3781f609c1aa0074cb60bbc9e3969495c
Sytraco <pointeau.simon@gmail.com>
2022-04-08 19:51:54 +0200

    application font colors

---
+++ configuration/style.sh
@@ -0,0 +1,10 @@
+# initial color
+none="\033[0m"
+# classical font color
+red="\033[0;31m"
+green="\033[0;32m"
+yellow="\033[0;33m"
+# bold font color
+redB="\033[1;31m"
+greenB="\033[1;32m"
+yellowB="\033[1;33m"
